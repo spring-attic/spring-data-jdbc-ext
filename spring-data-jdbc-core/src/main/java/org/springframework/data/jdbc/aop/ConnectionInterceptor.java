edc6a0815f53db730fba603e58dadcf951e832b5
Thomas Risberg <trisberg@vmware.com>
2010-11-09 16:43:18 -0500

    modified package names to org.springframework.data.jdbc...

---
+++ spring-data-jdbc-core/src/main/java/org/springframework/data/jdbc/aop/ConnectionInterceptor.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2008-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.data.jdbc.aop;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.data.jdbc.support.ConnectionPreparer;
+
+import java.sql.Connection;
+
+/**
+ * ConnectionInterceptor that delegates to a ConectionPreparer implementation.
+ *
+ * @author Thomas Risberg
+ * @since 1.0
+ */
+public class ConnectionInterceptor implements MethodInterceptor {
+
+    private ConnectionPreparer connectionPreparer;
+
+
+    /**
+     * Setter for the <code>ConnectionPreparer</code> implementation that will handle the
+     * preparation of the <code>Connection</code>.
+     * @param connectionPreparer the <code>ConnectionPreparer</code> implementation
+     */
+    public void setConnectionPreparer(ConnectionPreparer connectionPreparer) {
+        this.connectionPreparer = connectionPreparer;
+    }
+
+    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+        Object o = methodInvocation.proceed();
+        Object ret = o;
+        if (o != null) {
+            if (connectionPreparer != null) {
+                ret = connectionPreparer.prepare((Connection) o);
+            }
+        }
+        return ret;
+    }
+}
---
+++ spring-data-oracle/src/main/java/org/springframework/data/jdbc/jms/support/converter/oracle/MappingAdtMessageConverter.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2008-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.data.jdbc.jms.support.converter.oracle;
+
+import oracle.jms.AQjmsAdtMessage;
+import oracle.jms.AQjmsSession;
+import oracle.jms.AdtMessage;
+import oracle.sql.Datum;
+import oracle.sql.ORAData;
+import oracle.sql.STRUCT;
+
+import org.springframework.data.jdbc.jms.support.oracle.DatumMapper;
+import org.springframework.jms.support.converter.MessageConversionException;
+import org.springframework.jms.support.converter.MessageConverter;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * A <code>MessageConverter</code> that handles mapping between an ADT payload and a domain object.  Delegates
+ * the mapping to a <code>DatumMapper</code> implementation.
+ * 
+ * @author Thomas Risberg
+ * @since 1.0
+ */
+public class MappingAdtMessageConverter implements MessageConverter {
+
+    /* The datum mapper that will handle the mapping */
+    private DatumMapper mapper;
+
+    /**
+     * Constructor used to construct the <code>MessageConverter</code> and configure it with
+     * a <code>DatumMapper</code> implementation to be used.
+     * 
+     * @param mapper the <code>DatumMapper</code> implementation to be used
+     */
+    public MappingAdtMessageConverter(DatumMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    protected DatumMapper getMapper() {
+        return mapper;
+    }
+
+    public Message toMessage(final Object object, Session session) throws JMSException, MessageConversionException {
+        AdtMessage message = ((AQjmsSession)session).createAdtMessage();
+        message.setAdtPayload(new ORAData() {
+            public Datum toDatum(Connection conn) throws SQLException {
+                return getMapper().toDatum(object, conn);
+            }
+        });
+        return message;
+    }
+
+    public Object fromMessage(Message message) throws JMSException, MessageConversionException {
+        AQjmsAdtMessage aqMessage = (AQjmsAdtMessage) message;
+        Object result = null;
+        ORAData data = (ORAData) aqMessage.getAdtPayload();
+		try {
+			STRUCT struct = (STRUCT) data.toDatum(null);
+			result = getMapper().fromDatum(struct);
+		} catch (SQLException sqle) {
+			throw new MessageConversionException("Error while accessing ADT payload", sqle);
+		}
+		return result;
+    }
+    
+}
