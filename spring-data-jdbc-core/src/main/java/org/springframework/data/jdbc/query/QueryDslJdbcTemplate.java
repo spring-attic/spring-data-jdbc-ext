9524d0e3e899dc1d7e72026bce8cfaf098bde07a
Thomas Risberg <trisberg@vmware.com>
2011-10-18 08:17:16 -0400

    DATAJDBC-13 Adding JavaDocs

--- spring-data-jdbc-core/src/main/java/org/springframework/data/jdbc/query/QueryDslJdbcTemplate.java
+++ spring-data-jdbc-core/src/main/java/org/springframework/data/jdbc/query/QueryDslJdbcTemplate.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2008-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.data.jdbc.query;
 
 import java.sql.Connection;
@@ -37,6 +53,20 @@ import com.mysema.query.sql.dml.SQLUpdateClause;
 import com.mysema.query.types.Expression;
 import com.mysema.query.types.ExpressionBase;
 
+/**
+ * Template class with a basic set of JDBC operations, allowing the use
+ * of QueryDSL features.
+ *
+ * <p>This class delegates to a wrapped {@link #getJdbcOperations() JdbcTemplate}.
+ *
+ * <p>The underlying {@link org.springframework.jdbc.core.JdbcTemplate} is
+ * exposed to allow for convenient access to the traditional
+ * {@link org.springframework.jdbc.core.JdbcTemplate} methods.
+ *
+ * @author Thomas Risberg
+ * @since 1.0
+ * @see org.springframework.jdbc.core.JdbcTemplate
+ */
 public class QueryDslJdbcTemplate implements QueryDslJdbcOperations {
 
 	private JdbcTemplate jdbcTemplate;
--- spring-data-jdbc-core/src/main/java/org/springframework/data/jdbc/query/SqlUpdateCallback.java
+++ spring-data-jdbc-core/src/main/java/org/springframework/data/jdbc/query/SqlUpdateCallback.java
@@ -1,7 +1,33 @@
+/*
+ * Copyright 2008-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.data.jdbc.query;
 
 import com.mysema.query.sql.dml.SQLUpdateClause;
 
+/**
+ * An interface used by {@link QueryDslJdbcTemplate} for update calls
+ * where you are able to provide a {@link SQLUpdateClause} 
+ * implementation to handle the update logic.
+ *
+ * @author Thomas Risberg
+ * @since 1.0
+ * @see QueryDslJdbcTemplate
+ * @see SQLUpdateClause
+ */
 public interface SqlUpdateCallback {
 	
 	long doInSqlUpdateClause(SQLUpdateClause update);
